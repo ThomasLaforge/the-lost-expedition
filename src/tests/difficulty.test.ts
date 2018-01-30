8fefe13c8383e11adfc4beeb346727504070bf65
Thomas Laforge <thomas.laforge.38@gamil.com>
2018-01-30 17:50:04 +0100

    tests for difficulty

--- src/tests/difficulty.test.ts
+++ src/tests/difficulty.test.ts
@@ -3,39 +3,60 @@ import { Game } from '../modules/Game'
 import { Difficulty } from '../modules/TheLostExpedition'
 
 describe('Difficulty', () => {
-    describe('Difficulty: Hard', () => {    
 
+    // Facile : utilisez sept cartes d'expédition. Placez quatre jetons de santé sur chaque explorateur.
+    // Normal : utilisez neuf cartes d'expédition. Placez quatre jetons de santé sur chaque explorateur.
+    // Difficile : utilisez neuf cartes d'expédition. Placez trois jetons de santé sur chaque explorateur et prenez un jeton de nourriture supplémentaire. 
+
+    describe('Hard', () => {    
         let game = new Game(Difficulty.Hard)
-        let autoPlayer = game.player
         
-        describe('player atributes', () => {
-            describe('food stock', () => {
-                test('food stock initial', () => {
-                    expect(autoPlayer.foodStock.stockSize).toEqual(4);
-                })
-                test('eat', () => {      
-                    autoPlayer.eat();
-                    expect(autoPlayer.foodStock.stockSize).toEqual(3);
-                })
-                test('get food', () => {      
-                    autoPlayer.getFood();
-                    expect(autoPlayer.foodStock.stockSize).toEqual(4);
-                })
-            })
-            
-            describe('bullet stock', () => {
-                test('initial stock', () => {
-                    expect(autoPlayer.bulletStock.stockSize).toEqual(3);
-                })
-                test('shot', () => {
-                    autoPlayer.shot();
-                    expect(autoPlayer.bulletStock.stockSize).toEqual(2);
-                })
-                test('get a bullet', () => {      
-                    autoPlayer.getBullet();
-                    expect(autoPlayer.bulletStock.stockSize).toEqual(3);
-                })
-            })
+        test('food', () => {
+            expect(game.player.foodStock.stockSize).toEqual(4);
+        })
+        test('bullet', () => {
+            expect(game.player.bulletStock.stockSize).toEqual(3);
+        })
+        test('pv', () => {
+            expect(game.player.heroesCollection.heroes[0].pvStock.stockSize).toEqual(3);
+        })
+        test('road length', () => {
+            expect(game.road.length).toEqual(9);
+        })
+    })
+
+    describe('Normal', () => {    
+        let game = new Game(Difficulty.Normal)
+        
+        test('food', () => {
+            expect(game.player.foodStock.stockSize).toEqual(3);
+        })
+        test('bullet', () => {
+            expect(game.player.bulletStock.stockSize).toEqual(3);
+        })
+        test('pv', () => {
+            expect(game.player.heroesCollection.heroes[0].pvStock.stockSize).toEqual(4);
+        })
+        test('road length', () => {
+            expect(game.road.length).toEqual(9);
+        })
+    })
+
+    describe('Easy', () => {    
+
+        let game = new Game(Difficulty.Easy)
+        
+        test('food', () => {
+            expect(game.player.foodStock.stockSize).toEqual(3);
+        })
+        test('bullet', () => {
+            expect(game.player.bulletStock.stockSize).toEqual(3);
+        })
+        test('pv', () => {
+            expect(game.player.heroesCollection.heroes[0].pvStock.stockSize).toEqual(4);
+        })
+        test('road length', () => {
+            expect(game.road.length).toEqual(7);
         })
     })
 })
